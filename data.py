78d0d777465ac2cdf88a8bab126a6602d7478f21
LJP-Perfect <13106058519@163.com>
2018-12-09 23:01:52 +0800

    update data.py 1.0

--- data.py
+++ data.py
@@ -10,9 +10,12 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+import tkinter as tk
+from tkinter import messagebox
+
 count = 0
 max=17
-refreshtime=2
+refreshtime=30
 def getHTMLText(url):
     time.sleep(refreshtime)
     driver.get(url)  # 获取网页
@@ -59,11 +62,29 @@ def send_qq(to_who, msg):
     win32gui.SendMessage(qq, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
     win32gui.SendMessage(qq, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
 
+def hit_me():
+    #tc1=messagebox.askokcancel(title='hello',message='hello word') #弹窗 return True/False
+    #print (tc1)
+    #tc2=messagebox.askquestion(title='hello',message='hello word') #return yes/no
+    #print (tc2)
+    tc3=messagebox.showinfo(title='hello',message='hello word')  # return ok
+    print (tc3)
+
 
 
 
 if __name__ == '__main__':
-    driver = webdriver.Firefox()
+
+    window = tk.Tk()
+    window.title('my window')
+    window.geometry('400x200')
+
+    options = webdriver.Firefox()
+    options.add_argument('lang=zh_CN.UTF-8')
+    options.add_argument(
+        'user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"')
+    driver = webdriver.Firefox(firefox_options=options)
+    driver=webdriver.Safari()
     driver.minimize_window()
     length=0
     msg=''
@@ -75,6 +96,7 @@ if __name__ == '__main__':
             html = getHTMLText(url)  # 获取HTML
             tag = fillUnivlist(html)
             length=len(tag)
+
             if length==0:
                 zerocount+=1
             count+=1
@@ -84,7 +106,7 @@ if __name__ == '__main__':
                 driver.close()
                 time.sleep(3)
 
-                driver = webdriver.Firefox()
+                driver = webdriver.Chrome()
                 driver.minimize_window()
                 count=0
                 zerocount=0
@@ -95,9 +117,20 @@ if __name__ == '__main__':
     for i in range(max,length):
         content+=str(tag[i])
     soup = BeautifulSoup(str(content), 'html.parser')
-    to_who = 'Python宇宙第一'
+
     if msg=="error":
-        send_qq(to_who, "程序出错了，请重启")
+        print("error")
+        #普通弹框
+        tk.Message(window,text="程序出错了，请重启",width=400).pack()
+        window.mainloop()
+        #发送到qq窗口
+        #to_who = 'Python宇宙第一'
+        #send_qq(to_who, "程序出错了，请重启")
     else:
         msg= soup.find_all('h2')
-        send_qq(to_who, str(msg))
+        #普通弹框
+        tk.Message(window, text=str(msg), width=400).pack()
+        window.mainloop()
+        #发送到qq窗口
+        #to_who = 'Python宇宙第一'
+        #send_qq(to_who, str(msg))
